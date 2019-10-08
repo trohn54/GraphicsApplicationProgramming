4c3d87a430b6a549c0393cc0d78b8fdf64b1eec7
Thomas Rohn <trohn54@gmail.com>
2019-10-07 22:04:14 -0500

    Add Transform Assignment

---
+++ Transforms/Assets/Transforms.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transforms : MonoBehaviour
+{
+    float speed;
+    Vector3 temp;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = Mathf.Sin(.5f);
+
+        temp = transform.localScale;
+        temp.x += .03f;
+        transform.localScale = temp;
+        transform.Rotate(0,1000*speed * Time.deltaTime, 0);
+        transform.Translate(0, speed * Time.deltaTime, 0);
+    }
+}
